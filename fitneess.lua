a6a0df9fa1b3608a71053c1f00facf2374114f9c
ankushagrawal94 <ankushagrawal94@yahoo.com>
2015-07-18 08:13:41 -0700

    Code actually runs this time. Fitness function still a little jank

--- fitneess.lua
+++ fitneess.lua
@@ -4,13 +4,16 @@ function calcFitness()
   local highNum = highestNum()
   local mono = monotonicity()
   local numFree = freeTiles()
+
+  return highNum * 10 + mono + numFree
+
 end
 
 function highestNum()
   local highest = -1
-  for row in Grid do
-    if math.max(row) > highest then
-      highest = math.max(row)
+  for i = 1,tablelength(Grid) do
+    if max(Grid[i], function(a,b) return a < b end) > highest then
+      highest = max(Grid[i], function(a,b) return a < b end)
     end
   end
   return math.log10(highest)
@@ -20,8 +23,8 @@ function monotonicity()
 
   local totals = {0,0,0,0}
 
-  for x=1,5,1 do
-    local current = 0
+  for x=1,4,1 do
+    local current = 1
     local nextVal = current+1
     while nextVal < 4 do
       while nextVal < 4 and Grid[x][nextVal] == 0 do
@@ -39,7 +42,7 @@ function monotonicity()
       end
       local nextValue = -1
       if Grid[x][nextVal] ~= 0 then
-        nextValue = math.log10(Grid[x][current]) / math.log(2)
+        nextValue = math.log10(Grid[x][nextVal]) / math.log(2)
       end
       if Grid[x][nextVal] == 0 then
         nextValue = 0
@@ -55,8 +58,8 @@ function monotonicity()
     end
   end
 
-  for y=1,5,1 do
-    local current = 0
+  for y=1,4,1 do
+    local current = 1
     local nextVal = current+1
     while nextVal < 4 do
       while nextVal < 4 and Grid[nextVal][y] == 0 do
@@ -89,13 +92,16 @@ function monotonicity()
       nextVal = nextVal + 1
     end
   end
+
+  return math.max(totals[1], totals[2]) + math.max(totals[3], totals[4])
+
 end
 
-function freeTiles():
+function freeTiles()
   local count = 0
-  for row in Grid do
-    for val in row do
-      if val == 0 then
+  for i = 1,tablelength(Grid) do
+    for j=1,tablelength(Grid[i]) do
+      if j == 0 then
         count = count + 1
       end
     end
@@ -103,3 +109,21 @@ function freeTiles():
   return count
 end
 
+function tablelength(T)
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
+end
+
+function max(t, fn)
+    if #t == 0 then return nil, nil end
+    local key, value = 1, t[1]
+    for i = 2, #t do
+        if fn(value, t[i]) then
+            key, value = i, t[i]
+        end
+    end
+    return key, value
+end
+
+calcFitness()
